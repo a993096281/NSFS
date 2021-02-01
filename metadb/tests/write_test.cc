5119608ddbeee32486c568426082f99d44af94f6
a993096281 <993096281@qq.com>
2021-02-01 17:01:08 +0800

    test

--- metadb/tests/write_test.cc
+++ metadb/tests/write_test.cc
@@ -17,12 +17,11 @@ void DirRandomWrite(DB* db){
     char *fname = new char[8 + 1];
     inode_id_t value;
     uint64_t id = 0;
-    uint64_t bytes = 0;
     int ret = 0;
     for(int i = 0; i < nums; i++){
         //id = Random64(&seed);
         id = Random64(&seed) % nums;
-        key = 1;
+        key = id;
         value = id;
         snprintf(fname, 8 + 1, "%0*llx", 8, id);
         DBG_LOG("put:%d key:%lu fname:%.*s value:%lx hash_fname:%lx", i, key, 8, fname, value, MurmurHash64(fname, 8));
@@ -38,6 +37,42 @@ void DirRandomWrite(DB* db){
     delete fname;
 }
 
+void DirRandomRead(DB* db){
+    uint32_t seed = 1000;
+    uint64_t nums = 1000;
+
+    inode_id_t key;
+    char *fname = new char[8 + 1]; 
+    inode_id_t value;
+    uint64_t id = 0;
+    uint64_t found = 0;
+    int ret = 0;
+    for(int i = 0; i < nums; i++){
+        //id = Random64(&seed);
+        id = Random64(&seed) % nums;
+        key = id;
+        value = 0;
+        snprintf(fname, 8 + 1, "%0*llx", 8, id);
+
+        ret = db->DirGet(key, Slice(fname, 8), value);
+        if(ret != 0 && ret != 2){
+            fprintf(stderr, "dir get error! key:%lu fname:%.*s\n", key, 8, fname);
+            fflush(stderr);
+            exit(1);
+        }
+        if(ret == 0) {
+            if(value != key) fprintf(stdout, "dir get wrong value! key:%lu fname:%.*s value:%lu\n", key, 8, fname, value);
+            found++;
+        }
+        else{
+            fprintf(stdout, "dir read no found! key:%lu fname:%.*s\n", key, 8, fname);
+        }
+    }
+
+    delete fname;
+    printf("(%lu of %lu found)", found, nums);
+}
+
 int main(int argc, char *argv[])
 {
     DB *db;
@@ -51,6 +86,7 @@ int main(int argc, char *argv[])
     }
 
     DirRandomWrite(db);
+    DirRandomRead(db);
 
     delete db;
     return 0;
