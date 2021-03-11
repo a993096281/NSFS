758cf727f8db268ace50bed02669eb3146620891
a993096281 <993096281@qq.com>
2021-03-11 13:48:46 +0800

    test

---
+++ filebench/removedirs.f
@@ -0,0 +1,23 @@
+#
+# Creates a fileset with $ndirs empty leaf directories then rmdir's all of them
+#
+
+set $dir=/tmp
+set $ndirs=125
+set $meandirwidth=5
+set $nthreads=1
+
+set mode quit firstdone
+
+define fileset name=bigfileset,path=$dir,size=0,leafdirs=$ndirs,dirwidth=$meandirwidth,prealloc
+
+define process name=remdir,instances=1
+{
+  thread name=removedirectory,memsize=1m,instances=$nthreads
+  {
+    flowop removedir name=dirremover,filesetname=bigfileset
+  }
+}
+
+echo  "RemoveDir Version 1.0 personality successfully loaded"
+run 10
